19e3df3fa9ec2f812e20a351ea1f3e0f389539d7
samuelfsd <samuel.estudo778@gmail.com>
2023-07-27 19:54:46 -0300

    chore: add imports plugin

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -11,7 +11,7 @@ export const Groups = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#1c1c1c",
     alignItems: "center",
     justifyContent: "center",
   },
