31cff4e6efe370580556d13e4d32acbffa9d20ac
samuelfsd <samuel.estudo778@gmail.com>
2023-08-07 19:46:51 -0300

    feat: add roboto fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import theme from "@theme/index";
+
 import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
