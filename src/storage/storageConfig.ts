d428b5e8c97aa8302ec7f99dc0189ec2a33dd9cb
samuelfsd <samuel.estudo778@gmail.com>
2023-09-09 19:38:35 -0300

    feat: create players storage

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
-const GROUP_COLLECTION = "@invite-teams";
+const GROUP_COLLECTION = "@invite-teams:groups";
+const PLAYER_COLLECTION = "@invite-teams:players";
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
